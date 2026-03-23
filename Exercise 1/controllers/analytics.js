532631bdec18dc64740dac44d1c287cbbf92f916
VivekChudasama <vivek.ownai@gmail.com>
2026-03-23 18:09:02 +0530

    add status code in response of analytics

--- Exercise 1/controllers/analytics.js
+++ Exercise 1/controllers/analytics.js
@@ -1,7 +1,7 @@
 const reqCount = require('../services/analytics')
 
 exports.analytics = (req, res, next) => {
-    res.json({
+    res.status(200).json({
         reqCount : reqCount.getTotalAnalytics(),
         individualRequestCount : reqCount.getIndividualRequest(),
         totalAvgTime : reqCount.getTotaltime()
